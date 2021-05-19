2550b9921c0caee4417aee0625f756431c49e96a
syed bilal <79101571+Syed745@users.noreply.github.com>
2021-05-20 01:05:02 +0500

    Revert "20/5/2021"
    This reverts commit f628b8ef35627dae2aa32b5a0949d2485f648588.

--- script.js
+++ script.js
@@ -1,82 +1,51 @@
-// // // alert('Hello World');
+// // alert('Hello World');
 
-// // // // // // Data Types
+// // // // // Data Types
 
-// // // // // // 1. String
-// // // // // // "1000"
+// // // // // 1. String
+// // // // // "1000"
 
-// // // // // // 2. Number
-// // // // // // 123
+// // // // // 2. Number
+// // // // // 123
 
-// // // // // // 3. Boolean
-// // // // // // true
+// // // // // 3. Boolean
+// // // // // true
 
-// // // // // // 4. undefined
-// // // // // // undefined
+// // // // // 4. undefined
+// // // // // undefined
 
-// // // // // // 5. null
-// // // // // // null
+// // // // // 5. null
+// // // // // null
 
-// // // // // // 6. Object
-// // // // // /* {
-// // // // //   leafColor: "Green",
-// // // // //   branches: 4
-// // // // // } */
+// // // // // 6. Object
+// // // // /* {
+// // // //   leafColor: "Green",
+// // // //   branches: 4
+// // // // } */
 
-// // // // // var two=true;
-// // // // // var jkl='1000';
-// // // // // var xyz=5;
+// // // // var two=true;
+// // // // var jkl='1000';
+// // // // var xyz=5;
 
-// // // // // // var a=4;
-// // // // // // var b=9;
-// // // // // // var c=8;
-// // // // // // var k=false;
-// // // // // // var d=a+b+c-k;
+// // // // // var a=4;
+// // // // // var b=9;
+// // // // // var c=8;
+// // // // // var k=false;
+// // // // // var d=a+b+c-k;
 
-// // // // // alert(d)
-
-
-// // // // var x = 5;
-// // // // var y = "Ali";
-// // // // var z = y+x;
-// // // // alert(z);
+// // // // alert(d)
 
 
-// // // document.write("Saylani" + (5 + 5));
-
-// // // Pre -- Increment -- Post
-// // // Pre -- Decrement -- Post
-
-// // // var num = 8;
-// // // document.write(--num);
-// // // document.write('<br>');
-// // // document.write(num);
-
-// // // var num = 5;
-// // // console.log(num);
-// // // num /=5;
-// // // console.log(num);
-
-// // // var random =10 + Number('10.5') + 30;
-// // // console.log(random);
+// // // var x = 5;
+// // // var y = "Ali";
+// // // var z = y+x;
+// // // alert(z);
 
 
-// // var dahiAvailabe = false;
-// // var tomatoAvailable = true;
+// // document.write("Saylani" + (5 + 5));
 
-// // if (dahiAvailable) {
-// //     console.log("1 pao dahi dedo!");
-// // } else if (tomatoAvailable) {
-// //     console.log("1 pao.timatar dedo!");
-// // } else {
-// //     console.log("1 pao doodh dedo!");
-// // }
-// // // // // // // //
-// // document.write("Hello world")      
-// // document.write("Saylani" + (5 + 5));               
-
-// // // Pre -- Increment -- Post
-// // // Pre -- Decrement -- Post
+// // Pre -- Increment -- Post
+// // Pre -- Decrement -- Post
 
 // // var num = 8;
 // // document.write(--num);
@@ -85,82 +54,71 @@
 
 // // var num = 5;
 // // console.log(num);
-// // num /= 5;
+// // num /=5;
 // // console.log(num);
 
-// // var random = 10 + Number('10.5') + 30;
+// // var random =10 + Number('10.5') + 30;
 // // console.log(random);
 
-//  var dahiAvailable = false;
-//  var tomatoAvailable = true;
+
+// var dahiAvailabe = false;
+// var tomatoAvailable = true;
 
 // if (dahiAvailable) {
-//     console.log("1 Pao dahi dedo!");
+//     console.log("1 pao dahi dedo!");
 // } else if (tomatoAvailable) {
-//     console.log("1 Pao timatar dedo!");
+//     console.log("1 pao.timatar dedo!");
 // } else {
-//     console.log("1 Pao doodh dedo!");
+//     console.log("1 pao doodh dedo!");
 // }
-// var random = Math.random();
-// random = Math.round(random * 10);
-// var askedNum = prompt('Write a number');
-// askedNum = Number(askedNum);
+// // // // // // //
+// document.write("Hello world")      
+// document.write("Saylani" + (5 + 5));               
 
-// if (askedNum === random) {
-//     console.log("You Win!");
-// } else {
-//     console.log("You Lose!");
-// }
+// // Pre -- Increment -- Post
+// // Pre -- Decrement -- Post
+
+// var num = 8;
+// document.write(--num);
+// document.write('<br>');
+// document.write(num);
+
+// var num = 5;
+// console.log(num);
+// num /= 5;
+// console.log(num);
+
+// var random = 10 + Number('10.5') + 30;
 // console.log(random);
-// console.log(askedNum);
-// console.log(3 ** 3);
 
+ var dahiAvailable = false;
+ var tomatoAvailable = true;
 
-// var num = 2;
-// num++;
-// ++num;
-// console.log(num--);
-// console.log 
-
-// var num = Number(prompt("Enter a number"));
-// if (isNaN(num)) {
-//     console.log("This is not a number");
-// } else if (num % 2 === 0 ) {
-//     console.log ("This number is Even");
-// } else {
-//     console.log("This number is Odd");
-// }
-
-// var num = Number(prompt("Enter a Number"));
-// //  num = 6; if divisible by 3 print Fizz
-// // num = 10; if divisible by 5 print Buzz
-// // num = 15; if divisible by 3&5 print FizzBuzz
-// // num = 8; Print num
-// if (num % 3 === 0 && num % 5 === 0) {
-//      console.log("FizzBuzz");
-
-//  } else if (num % 5 === 0) {
-//      console.log("Buzz");
-//  } else if (num % 3 === 0) {
-//     console.log("Fizz");
-//  } else {
-//      console.log(num);
-//  }
-
-// True: true,"a" , 1, 1-9, 
-// False: false, "" , 0 , null , undefined
-
-
-// console.log(false && false);
-
-// 1. initialization
-// 2. condition
-// 3. Increment
-
-var num = 15;
-for (var i = 1; i <= 10; i++) {
-    console.log(num + " X " + i +" = " + num*i);
+if (dahiAvailable) {
+    console.log("1 Pao dahi dedo!");
+} else if (tomatoAvailable) {
+    console.log("1 Pao timatar dedo!");
+} else {
+    console.log("1 Pao doodh dedo!");
 }
+var random = Math.random();
+random = Math.round(random * 10);
+var askedNum = prompt('Write a number');
+askedNum = Number(askedNum);
+
+if (askedNum === random) {
+    console.log("You Win!");
+} else {
+    console.log("You Lose!");
+}
+console.log(random);
+console.log(askedNum);
+console.log(3 ** 3);
+
+
+
+
+
 
 
 
