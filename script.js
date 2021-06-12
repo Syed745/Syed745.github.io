855c653e1a6d1cb421c486636c1cb823f2224825
Syed745 <79101571+Syed745@users.noreply.github.com>
2021-06-11 21:17:46 -0700

    example

--- script.js
+++ script.js
@@ -157,11 +157,30 @@
 // 2. condition
 // 3. Increment
 
-var num = 15;
-for (var i = 1; i <= 10; i++) {
-    console.log(num + " X " + i +" = " + num*i);
+// var num = 15;
+// for (var i = 1; i <= 10; i++) {
+//     console.log(num + " X " + i +" = " + num*i);
+// }
+
+var x = 10;
+
+function createFunction1() {
+    var x = 20;
+    return new Function('return x;'); // this |x| refers global |x|
 }
 
+function createFunction2() {
+    var x = 20;
+    function f() {
+        return x; // this |x| refers local |x| above
+    }
+    return f;
+}
+
+var f1 = createFunction1();
+console.log(f1());          // 10
+var f2 = createFunction2();
+console.log(f2());          // 20s
 
 
 
