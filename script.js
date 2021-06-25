860f35ebd13fbb2c61e35ed8da604cdeb1d4046a
Syed745 <79101571+Syed745@users.noreply.github.com>
2021-06-25 10:36:54 -0700

    sample

--- script.js
+++ script.js
@@ -1,245 +1,174 @@
-// // // alert('Hello World');
-
-// // // // // // Data Types
-
-// // // // // // 1. String
-// // // // // // "1000"
-
-// // // // // // 2. Number
-// // // // // // 123
-
-// // // // // // 3. Boolean
-// // // // // // true
-
-// // // // // // 4. undefined
-// // // // // // undefined
-
-// // // // // // 5. null
-// // // // // // null
-
-// // // // // // 6. Object
-// // // // // /* {
-// // // // //   leafColor: "Green",
-// // // // //   branches: 4
-// // // // // } */
-
-// // // // // var two=true;
-// // // // // var jkl='1000';
-// // // // // var xyz=5;
-
-// // // // // // var a=4;
-// // // // // // var b=9;
-// // // // // // var c=8;
-// // // // // // var k=false;
-// // // // // // var d=a+b+c-k;
-
-// // // // // alert(d)
-
-
-// // // // var x = 5;
-// // // // var y = "Ali";
-// // // // var z = y+x;
-// // // // alert(z);
-
-
-// // // document.write("Saylani" + (5 + 5));
-
-// // // Pre -- Increment -- Post
-// // // Pre -- Decrement -- Post
-
-// // // var num = 8;
-// // // document.write(--num);
-// // // document.write('<br>');
-// // // document.write(num);
-
-// // // var num = 5;
-// // // console.log(num);
-// // // num /=5;
-// // // console.log(num);
-
-// // // var random =10 + Number('10.5') + 30;
-// // // console.log(random);
-
-
-// // var dahiAvailabe = false;
-// // var tomatoAvailable = true;
-
-// // if (dahiAvailable) {
-// //     console.log("1 pao dahi dedo!");
-// // } else if (tomatoAvailable) {
-// //     console.log("1 pao.timatar dedo!");
-// // } else {
-// //     console.log("1 pao doodh dedo!");
-// // }
-// // // // // // // //
-// // document.write("Hello world")      
-// // document.write("Saylani" + (5 + 5));               
-
-// // // Pre -- Increment -- Post
-// // // Pre -- Decrement -- Post
-
-// // var num = 8;
-// // document.write(--num);
-// // document.write('<br>');
-// // document.write(num);
-
-// // var num = 5;
-// // console.log(num);
-// // num /= 5;
-// // console.log(num);
-
-// // var random = 10 + Number('10.5') + 30;
-// // console.log(random);
-
-//  var dahiAvailable = false;
-//  var tomatoAvailable = true;
-
-// if (dahiAvailable) {
-//     console.log("1 Pao dahi dedo!");
-// } else if (tomatoAvailable) {
-//     console.log("1 Pao timatar dedo!");
-// } else {
-//     console.log("1 Pao doodh dedo!");
-// }
-// var random = Math.random();
-// random = Math.round(random * 10);
-// var askedNum = prompt('Write a number');
-// askedNum = Number(askedNum);
-
-// if (askedNum === random) {
-//     console.log("You Win!");
-// } else {
-//     console.log("You Lose!");
-// }
-// console.log(random);
-// console.log(askedNum);
-// console.log(3 ** 3);
-
-
-// var num = 2;
-// num++;
-// ++num;
-// console.log(num--);
-// console.log 
-
-// var num = Number(prompt("Enter a number"));
-// if (isNaN(num)) {
-//     console.log("This is not a number");
-// } else if (num % 2 === 0 ) {
-//     console.log ("This number is Even");
-// } else {
-//     console.log("This number is Odd");
-// }
-
-// var num = Number(prompt("Enter a Number"));
-// //  num = 6; if divisible by 3 print Fizz
-// // num = 10; if divisible by 5 print Buzz
-// // num = 15; if divisible by 3&5 print FizzBuzz
-// // num = 8; Print num
-// if (num % 3 === 0 && num % 5 === 0) {
-//      console.log("FizzBuzz");
-
-//  } else if (num % 5 === 0) {
-//      console.log("Buzz");
-//  } else if (num % 3 === 0) {
-//     console.log("Fizz");
-//  } else {
-//      console.log(num);
-//  }
-
-// True: true,"a" , 1, 1-9, 
-// False: false, "" , 0 , null , undefined
-
-
-// console.log(false && false);
-
-// 1. initialization
-// 2. condition
-// 3. Increment
-
-// var num = 15;
-// for (var i = 1; i <= 10; i++) {
-//     console.log(num + " X " + i +" = " + num*i);
-// }
-
 // var x = 10;
 
-// function createFunction1() {
+// function myFunc1 () {
 //     var x = 20;
-//     return new Function('return x;'); // this |x| refers global |x|
+//     return new Function('return x;');
 // }
 
-// function createFunction2() {
+// function myFunc2() {
 //     var x = 20;
-//     function f() {
-//         return x; // this |x| refers local |x| above
+//     function f () {
+//         return x; 
 //     }
 //     return f;
 // }
 
-// var f1 = createFunction1();
-// console.log(f1());          // 10
-// var f2 = createFunction2();
-// console.log(f2());          // 20s
+// var f1 = myFunc1();
+// console.log (f1());
+// var f2 = myFunc2();
+// console.log(f2());
 
-// i = 8;
-// i --
-// i = 7
-// i = i-2
-// i = 5
-// i -= 2
-
-// for (var i = 10; i > 0; i-=2) {
-//     console.log("hello user " + i);
+// function myFunc (theObject) {
+//     theObject.brand ="Toyota";
 // }
 
-// Functions
+// var mycar = {
+//     brand: "Honda",
+//     model: "Accord",
+//     year: 1998
+// };
 
-//  function add (num1, num2) {
-//     return num1  + num2;
-//  }
+// console.log(mycar.brand);
 
-//  function substract (num1, num2) {
-//     return num1  - num2;
-//  }   
+// myFunc (mycar);
 
-// var result = add (17, 3);
-// var result2 = substract (result, 5);
-// console.log(result2);
+// let array = [1, 2, 3];
 
-// ARRAY 
-
-// var months = [
-//     "January",
-//     "Feburary",
-//     "March",
-// ];
- 
-// console.log(months);
-
-// var arr = [1,2,3,4];
-//arr.splice(0, 1);
-//arr.shift();
-//console.log(arr.slice(1,3));
-
-// var arr = ["Apple", "Mango", "Banana", "Orange" ];
-// var found = false;
-// for (var i = 0; i < arr.length; i++) {
-//     console.log ("Finding in "  + i)
-//     if (arr[i] === "Apple1"){
-//         console.log("Found");
-//         found = true;
-//         break;
+// function removeFirst(arr) {
+//     let array = [...arr];
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++)
+//     {
+//         array[i] = array[i+1]
+//         if (array[i] || undefined) {
+//             newArray.push(array[i])
+//         }
 //     }
+//     return newArray;
+// }
+// console.log(removeFirst(array));
+// var date = new Date();
+// // console.log(date.toString());
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// console.log(dayNames[date.getDay()]);
+// console.log(date.getDate());
+// var monthsNames = [];
+// console.log(date.getMonth());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+
+
+// function agecalc(){
+// var dob = new Date(prompt("Enter your date of birth", "Jan 26, 1997"));
+// var dobmili = dob.getTime();
+// var today = new Date ();
+// var todaymili = today.getTime();
+// var diff = todaymili - dobmili;
+// var accuage = Math.floor(diff/(1000*60*60*24*30*12));
+// alert(accuage);
+// }
+// agecalc();
+// function add(a,b,c){
+//     var z = a+b+c
+//     return z;
 // }
 
-// if (found){
-//     console.log("Found");
-// }
-
-var date = prompt("Enter your birth date");
-var date = Date() ;
-
-console.log("num");
+// var g = add(2,5,1);
+// alert(g);
 
 
+let userDate = prompt("Enter your date of birth");
+let userMonth = prompt("Enter your month of birth").toLowerCase();
+let userYear = prompt("Enter your year of birth");
+
+let DOB = new Date();
+DOB.setDate(userDate);
+DOB.setFullYear(userYear);
+let currentDate = new Date ();
+
+if ( userMonth === "1" || userMonth === "jan" || userMonth === "january") {
+    DOB.setMonth(0);
+}
+
+else if ( userMonth === "2" || userMonth === "feb" || userMonth === "february") {
+    DOB.setMonth(1);
+}
+
+else if ( userMonth === "3" || userMonth === "mar" || userMonth === "march") {
+    DOB.setMonth(2);
+}
+
+else if ( userMonth === "4" || userMonth === "apr" || userMonth === "april") {
+    DOB.setMonth(3);
+}
+
+else if ( userMonth === "5" || userMonth === "may" || userMonth === "may") {
+    DOB.setMonth(4);
+}
+
+else if ( userMonth === "6" || userMonth === "jun" || userMonth === "june") {
+    DOB.setMonth(5);
+}
+
+else if ( userMonth === "7" || userMonth === "jul" || userMonth === "july") {
+    DOB.setMonth(6);
+}
+
+else if ( userMonth === "8" || userMonth === "aug" || userMonth === "august") {
+    DOB.setMonth(7);
+}
+
+else if ( userMonth === "9" || userMonth === "sep" || userMonth === "september") {
+    DOB.setMonth(8);
+}
+
+else if ( userMonth === "10" || userMonth === "oct" || userMonth === "octuber") {
+    DOB.setMonth(9);
+}
+
+else if ( userMonth === "11" || userMonth === "nov" || userMonth === "november") {
+    DOB.setMonth(10);
+}
+
+else if ( userMonth === "12" || userMonth === "dec" || userMonth === "december") {
+    DOB.setMonth(11);
+}
+
+else {
+    alert("please enter right date of birth ")
+}
+
+let diffMonth = currentDate.getMonth() - DOB.getMonth;
+
+let diffYear = currentDate.getFullYear() - DOB.getFullYear;
+
+if (DOB.getMonth() < currentDate.getMonth()) {
+    diffYear ++;
+}
+
+if (currentDate.getDate() === DOB.getDate() && currentDate.getMonth() === DOB.getMonth()) {
+    alert(diffYear + "years")
+} else if (currentDate.getMonth() === DOB.getMonth() && currentDate.getDate() < DOB.getDate()) {
+    document.write(diffYear + "years")
+}
+
+else {
+    document.write(diffYear -1 + "years")
+}
+
+if (currentDate.getMonth() < DOB.getMonth()) {
+    document.write(12 + diffMonth + " month & ")
+}
+
+else {
+    document.write(diffMonth + "month & ")
+}
+
+
+let diffDate = currentDate.getDate() - DOB.getDate();
+
+if (currentDate.getDate() > DOB.getDate()) {
+    document.write(diffDate + "days")
+} else {
+    document.write(DOB.getDate() - currentDate.getDate() + "days")
+}
