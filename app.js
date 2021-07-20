454d57830af716ae5ca8893a6cbba3f5c9708447
Syed745 <79101571+Syed745@users.noreply.github.com>
2021-07-20 10:02:34 -0700

    assignment
    objects

---
+++ app.js
@@ -0,0 +1,37 @@
+// function getElements(){
+//   //  var p = document.getElementsByTagName('p')
+//     console.log(document.childNodes[1])
+// }
+
+var person = {
+  firstName: "syed bilal",lastName: "ali",
+  email: "syedb1997@gmail.com",
+  password: "*******",
+  age: 24,
+  gender: "male",
+  city: "khi",
+  country: "pakistan"
+  
+// }
+// console.log(person)
+
+// function Plan(theOttomanEmpire, is, gretest, bil){
+//   this.theOttomanEmpire = theOttomanEmpire;
+//   this.is = is;
+//   this.gretest = gretest;
+//   this.bil = bil;
+// }
+// var plan1 = new Plan(654, 46567, 3456, 3456);
+// var plan2 = new Plan(12312, 7890, 09890, 786545);
+// console.log(plan1, plan2);
+
+// var itemsArray = [
+//   {name:"juice",price:"50", quantity:"3"},
+//   {name:"cookie",price:"30", quantity:"9"},
+//   {name:"shirt",price:"880", quantity:"1"},
+//   {name:"pen",price:"100", quantity:"2"}];
+//   function isPrice(fruit) {
+//     return fruit.price === "juice", "cookie", "shirt", "pen";
+//   }
+//   console.log(itemsArray.find(isPrice));
+  
