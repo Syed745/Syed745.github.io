e6af96c6a3f16a0423c0602108902c929374d934
Syed745 <79101571+Syed745@users.noreply.github.com>
2021-08-07 05:54:11 -0700

    cv

--- port.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scrollY(function(){
-        if(this.scrollY > 20){
-            $('.navber').addClass("sticky");
-        }else{
-            $('.navber').removeClass("sticky");
-        }
-    })
-});
